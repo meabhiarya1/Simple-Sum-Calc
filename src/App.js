4410a16131a5fd743cbcfb106f5ea39472c76603
Abhishek kumar <abhi.arya97161@gmail.com>
2023-07-26 22:28:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const[num1,setNum1] = useState();
+  const[num2,setNum2] = useState();
+  const[Add,setAdd] = useState();
+
+  const FirstHandleChange = (e) =>{
+    // console.log(e.target.value)
+    setNum1(e.target.value);
+  }
+
+  const SecondHandleChange = (e) =>{
+    // console.log(e.target.value)
+    setNum2(e.target.value);
+  }
+
+  const sum = (e) =>{
+    e.preventDefault();
+    setAdd(parseInt(num1)+parseInt(num2));
+    // console.log((parseInt(num1)+parseInt(num2)))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='container'>
+        <h2 className='center'>Sum Calculator</h2>
+        <form onSubmit={sum}>
+          <div>
+            <label>First Number</label>
+            <input type='number' 
+            placeholder='Enter First Number' 
+            value ={num1} 
+            onChange={FirstHandleChange}></input>
+          </div>
+
+          <div>
+            <label>Second Number</label>
+            <input type='number' 
+            placeholder='Enter Second Number' 
+            value ={num2} 
+            onChange={SecondHandleChange}></input>
+          </div>
+
+          <div>
+            <button className='btn' type='submit'>Submit</button>
+          </div>  
+
+          <div className='center'>
+            <h3>Your Sum is: {Add}</h3>
+          </div>
+
+        </form>
+
+      </div>
     </div>
   );
 }
